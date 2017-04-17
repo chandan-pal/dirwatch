e77ecfa9d1a5e60eef7a18ade1b14b2fcb9528b1
Sambuddha Bishee <da.animator@gmail.com>
2017-04-17 23:57:57 +0530

    Added recursive archiving and try-catch

--- archive.py
+++ archive.py
@@ -3,11 +3,18 @@
     Archives old directories into a predefined dir within current dir
 """
 # imports
-import os
-import datetime
+import os, datetime, shutil
+# Func Defs
+def overcopy(from_path, to_path):
+    """
+        copies dir trees forcefully
+    """
+    if os.path.exists(to_path):
+        shutil.rmtree(to_path)
+    shutil.copytree(from_path, to_path)
 # Parms
-dir_path = '/home/sambuddha/Dungeon/Code/python/dirwatch/trgt/'
-expir_days = 30
+dir_path = '/home/sambuddha/Dungeon/Code/python/github/dirwatch/trgt/'
+expir_days = 120
 archive_dir = 'ARCHIVE'
 # Timestamp
 currtime = datetime.datetime.now()
@@ -17,12 +24,15 @@ entryItr = os.scandir(dir_path)
 for iEntry in entryItr:
     print('DIR: ' + iEntry.name)
     if iEntry.name != archive_dir:
-        modtime = datetime.datetime.fromtimestamp(iEntry.stat().st_mtime)
+        modtime = datetime.datetime.fromtimestamp(iEntry.stat().st_atime)
         dirAge = currtime - modtime
         # Check if older than expiration
         if dirAge.days > expir_days:
-            # Copy directory and contents into archive_dir
-        print('Dir "{}" last modified on {}; {} days old.'.format(iEntry.name, modtime.strftime('%d-%b-%Y %H:%M:%S'), dirAge.days))
-    else:
-        print('archive')
+            try:
+                # Copy directory and contents into archive_dir
+                overcopy(iEntry.path, os.path.join(dir_path, archive_dir, iEntry.name))
+                shutil.rmtree(iEntry.path)
+            except Exception as ex:
+                print('ERROR backing up dir \'{}\': {}'.format(iEntry.name, str(ex)))
+        # print('Dir "{}" last modified on {}; {} days old.'.format(iEntry.name, modtime.strftime('%d-%b-%Y %H:%M:%S'), dirAge.days))
 # Main block END
