1c3e297942e7b6583114b3c96b068c1f658ce0c4
Sambuddha Bishee <da.animator@gmail.com>
2017-04-19 20:45:32 +0530

    Updated archive to gracefully handle conflicts and not force-overwrite

--- archive.py
+++ archive.py
@@ -2,23 +2,30 @@
     archive.py
     Archives old directories into a predefined dir within current dir
 """
-# imports
-import os, datetime, shutil
-# Func Defs
-def overcopy(from_path, to_path):
-    """
-        copies dir trees forcefully
-    """
-    if os.path.exists(to_path):
-        shutil.rmtree(to_path)
-    shutil.copytree(from_path, to_path)
+
 # Parms
 dir_path = '/home/sambuddha/Dungeon/Code/python/github/dirwatch/trgt/'
 expir_days = 120
 archive_dir = 'ARCHIVE'
+
+# imports
+import os, datetime, shutil
+
+# Func Defs
+def overcopy(from_path, to_path):
+    """
+        copies dir trees gracefully
+    """
+    print('in method')
+    if os.path.exists(to_path):
+        fpath, fname = os.path.split(to_path)
+        # Append timestamp to target path
+        to_path = os.path.join(fpath, '{}_{}'.format(fname, str(datetime.date.today())))
+    shutil.copytree(from_path, to_path)
+
+# Main block START
 # Timestamp
 currtime = datetime.datetime.now()
-# Main block START
 # Scan through subdirs
 entryItr = os.scandir(dir_path)
 for iEntry in entryItr:
@@ -27,6 +34,7 @@ for iEntry in entryItr:
         modtime = datetime.datetime.fromtimestamp(iEntry.stat().st_atime)
         dirAge = currtime - modtime
         # Check if older than expiration
+        # if dirAge.seconds > expir_days:
         if dirAge.days > expir_days:
             try:
                 # Copy directory and contents into archive_dir
